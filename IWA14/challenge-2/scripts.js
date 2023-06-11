2ee03554395ea4314e1fe1c7a0885a7b863a3d37
moses molepo <m.molepo@lab.clicklearning.org>
2023-06-11 19:07:24 +0200

    iwa14 challenge2

---
+++ IWA14/challenge-2/scripts.js
@@ -0,0 +1,41 @@
+// script.js
+
+const add = (a, b) => console.log(a + b);
+
+const multiply = (a, b, c) => console.log(a * (b + c));
+
+function internal() {
+  const added = add(4, 8);
+  const multiplied = multiply(8, 4, 2);
+  console.log(added);
+  console.log(multiplied);
+}
+
+internal();
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
